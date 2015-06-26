bf6c86182a2415a4526fe7b8aa350d6216665429
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-26 22:03:10 +0200

    Requires authorization to result in throwing an UnauthorizeException.java

--- app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
+++ app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
-import junit.framework.TestCase;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,21 +12,30 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
+ * Project: Popular Movies
  * Created by muhammad on 25/06/15.
  */
 public class ServerConnectionTest extends AndroidTestCase {
 
-    public void testRetrievingMoviesDataFromServerWithoutKeyWillResultInUnauthorizedResponse() throws Exception {
-        assertNull("Unauthorized response resulted in null value", getData(getContext(), ""));
-    }
 
+    public void testRetrievingMoviesDataWithoutProperKeyWillResultInUnauthorizedException() {
+        try {
+            String incorrectApiKey = "";
+            getData(getContext(), incorrectApiKey);
+            fail("Expected an Exception to be thrown");
+        } catch (UnauthorizedException ex) {
+            // Nothing to be done... test passed
+        } catch (IOException e) {
+            fail("Expected Unauthorized Exception but found IOException");
+        }
+    }
 
     public void testRetrievingMoviesDataFromServerReturnsResult() throws Exception {
         String apikey = getContext().getString(R.string.server_api_key);
         assertNotSame("Data retrieved is not empty", 0, getData(getContext(), apikey).length());
     }
 
-    private static String getData(Context context, String apiKey) throws IOException {
+    private static String getData(Context context, String apiKey) throws IOException, UnauthorizedException {
         String baseUrl = context.getString(R.string.server_base_url);
         Uri uri = Uri.parse(baseUrl).buildUpon()
                 .appendQueryParameter("sort_by", "popularity.desc")
@@ -37,7 +44,7 @@ public class ServerConnectionTest extends AndroidTestCase {
         HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(uri.toString()).openConnection();
         httpURLConnection.connect();
 
-        int responseCode = 0;
+        int responseCode;
         try {
             responseCode = httpURLConnection.getResponseCode();
         } catch (IOException e) {
@@ -47,18 +54,19 @@ public class ServerConnectionTest extends AndroidTestCase {
         switch (responseCode) {
             case HttpURLConnection.HTTP_OK:
                 InputStream inputStream = httpURLConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
+                StringBuilder stringBuilder = new StringBuilder();
 
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
+                    stringBuilder.append(line);
+                    stringBuilder.append("\n");
                 }
-                return buffer.toString();
+                return stringBuilder.toString();
             case HttpURLConnection.HTTP_UNAUTHORIZED:
-                return null;
+                throw new UnauthorizedException();
             default:
-                throw new IllegalStateException("Connection method is not equiped to handle this case");
+                throw new IllegalStateException("Connection method is not equipped to handle this case");
         }
     }
 
