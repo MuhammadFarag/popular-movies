5f7e50c266d071d3e6a8682523cabb23eab456d9
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-26 21:04:51 +0200

    Added test case to ensure that the data set returned from the server is not empty

--- app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
+++ app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
@@ -4,7 +4,10 @@ import android.test.AndroidTestCase;
 
 import junit.framework.TestCase;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -39,4 +42,21 @@ public class ServerConnectionTest extends AndroidTestCase {
         }
         assertEquals("Successful connection test, please make sure that the api_key is properly set",HttpURLConnection.HTTP_OK, responseCode);
     }
+
+    public void testRetrievingMoviesDataFromServerReturnsResult() throws Exception{
+        String url = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
+        String apikey = getContext().getString(R.string.server_api_key);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url + "&api_key=" + apikey).openConnection();
+        httpURLConnection.connect();
+        InputStream inputStream = httpURLConnection.getInputStream();
+        StringBuffer buffer = new StringBuffer();
+        assertNotNull(inputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line + "\n");
+        }
+        assertNotSame("Data retrieved is not empty",0,buffer.length());
+    }
+
 }
