073d3bd8b1462480d8a1fac0bd2fd9b8fde51d45
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-27 00:09:31 +0200

    Added test for retrieval of information required for the first movie in the result set

--- app/src/androidTest/java/com/muhammadfarag/popularmovies/DataParsingTest.java
+++ app/src/androidTest/java/com/muhammadfarag/popularmovies/DataParsingTest.java
@@ -10,17 +10,7 @@ import org.json.JSONObject;
  * Project: Popular Movies
  * Created by muhammad on 26/06/15.
  */
-public class DataParsingTest extends AndroidTestCase{
-
-
-    public void testCanRetrieveUpperLevelFields() throws JSONException {
-        String testData = getContext().getString(R.string.server_example_payload);
-        JSONObject jsonTestData = new JSONObject(testData);
-        jsonTestData.getJSONArray("results");
-        jsonTestData.getInt("page");
-        jsonTestData.getInt("total_pages");
-        jsonTestData.getInt("total_results");
-    }
+public class DataParsingTest extends AndroidTestCase {
 
     public void testCanRetrievePageNumber() throws JSONException {
         String testData = getContext().getString(R.string.server_example_payload);
@@ -40,7 +30,7 @@ public class DataParsingTest extends AndroidTestCase{
         String testData = getContext().getString(R.string.server_example_payload);
         JSONObject jsonTestData = new JSONObject(testData);
         int pageNumber = jsonTestData.getInt("total_results");
-        assertEquals("total number of results",231676,pageNumber);
+        assertEquals("total number of results", 231676, pageNumber);
     }
 
     public void testCheckSizeOfRetrievedResults() throws JSONException {
@@ -48,7 +38,19 @@ public class DataParsingTest extends AndroidTestCase{
         JSONObject jsonTestData = new JSONObject(testData);
         JSONArray results = jsonTestData.getJSONArray("results");
         int pageNumber = results.length();
-        assertEquals("Number of results per page",20,pageNumber);
+        assertEquals("Number of results per page", 20, pageNumber);
+    }
+
+    public void testRetrievalOfFirstMovieData() throws JSONException {
+        String testData = getContext().getString(R.string.server_example_payload);
+        JSONObject jsonTestData = new JSONObject(testData);
+        JSONArray results = jsonTestData.getJSONArray("results");
+        JSONObject firstMovieJson = results.getJSONObject(0);
+        assertEquals("Original Title", "Jurassic World", firstMovieJson.getString("original_title"));
+        assertEquals("Poster path", "/uXZYawqUsChGSj54wcuBtEdUJbh.jpg", firstMovieJson.getString("poster_path"));
+        assertEquals("Plot synopsis", "Twenty-two years after the events of Jurassic Park, Isla Nublar now features a fully functioning dinosaur theme park, Jurassic World, as originally envisioned by John Hammond.", firstMovieJson.getString("overview"));
+        assertEquals("User Rating", 7d, firstMovieJson.getDouble("vote_average"));
+        assertEquals("Release Date", "2015-06-12", firstMovieJson.getString("release_date"));
     }
 
 }
