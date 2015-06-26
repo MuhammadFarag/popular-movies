21062415138006eae98bf85013c01d837716ba09
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-26 15:52:06 +0200

    Added a resource file server-api-key.xml to contain the api key. The file currently contains a *dummy-api-key*. The actual key should not be committed. Changed the ServerConnectionTest.java to extend from AndroidTestCase instead of TestCase to have access to context and thus access to the created resource file Changed README.md file to reflect mentioned changes

--- app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
+++ app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
@@ -1,5 +1,7 @@
 package com.muhammadfarag.popularmovies;
 
+import android.test.AndroidTestCase;
+
 import junit.framework.TestCase;
 
 import java.io.IOException;
@@ -9,7 +11,7 @@ import java.net.URL;
 /**
  * Created by muhammad on 25/06/15.
  */
-public class ServerConnectionTest extends TestCase {
+public class ServerConnectionTest extends AndroidTestCase {
 
     public void testRetrievingMoviesDataFromServerWithoutKeyWillResultInUnauthorizedResponse() throws Exception {
         String url = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
@@ -22,5 +24,10 @@ public class ServerConnectionTest extends TestCase {
             responseCode = httpURLConnection.getResponseCode();
         }
         assertEquals(HttpURLConnection.HTTP_UNAUTHORIZED, responseCode);
+    }
+
+    public void testRetrivalOfApiKeyFromResourceFile(){
+        String apikey = getContext().getString(R.string.server_api_key);
+        assertEquals("Dummy Api Key retrival","dummy-api-key",apikey);
     }
 }
