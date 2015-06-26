7c993d53262e6c8dc6198ffba43ccb8836134e5b
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-26 23:14:09 +0200

    Cleaning up: Created MovieDatabaseServerConnector.java which handles connecting to server and throws UnauthorizedException.java if the server responds with HTTP Unauthorized (401), which usually happens if API key is not correct

--- app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
+++ app/src/androidTest/java/com/muhammadfarag/popularmovies/ServerConnectionTest.java
@@ -1,15 +1,6 @@
 package com.muhammadfarag.popularmovies;
 
-import android.content.Context;
-import android.net.Uri;
 import android.test.AndroidTestCase;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Project: Popular Movies
@@ -17,58 +8,11 @@ import java.net.URL;
  */
 public class ServerConnectionTest extends AndroidTestCase {
 
-
-    public void testRetrievingMoviesDataWithoutProperKeyWillResultInUnauthorizedException() {
-        try {
-            String incorrectApiKey = "";
-            getData(getContext(), incorrectApiKey);
-            fail("Expected an Exception to be thrown");
-        } catch (UnauthorizedException ex) {
-            // Nothing to be done... test passed
-        } catch (IOException e) {
-            fail("Expected Unauthorized Exception but found IOException");
-        }
-    }
+    //TODO: Create a test to test exception thrown when unauthorized (previously was tested by a removed stepping stone test
 
     public void testRetrievingMoviesDataFromServerReturnsResult() throws Exception {
-        String apikey = getContext().getString(R.string.server_api_key);
-        assertNotSame("Data retrieved is not empty", 0, getData(getContext(), apikey).length());
+        MovieDatabaseServerConnector connector = new MovieDatabaseServerConnector(getContext());
+        assertNotSame("Data retrieved is not empty", 0, connector.getData().length());
     }
-
-    private static String getData(Context context, String apiKey) throws IOException, UnauthorizedException {
-        String baseUrl = context.getString(R.string.server_base_url);
-        Uri uri = Uri.parse(baseUrl).buildUpon()
-                .appendQueryParameter("sort_by", "popularity.desc")
-                .appendQueryParameter("api_key", apiKey).build();
-
-        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(uri.toString()).openConnection();
-        httpURLConnection.connect();
-
-        int responseCode;
-        try {
-            responseCode = httpURLConnection.getResponseCode();
-        } catch (IOException e) {
-            responseCode = httpURLConnection.getResponseCode();
-        }
-
-        switch (responseCode) {
-            case HttpURLConnection.HTTP_OK:
-                InputStream inputStream = httpURLConnection.getInputStream();
-                StringBuilder stringBuilder = new StringBuilder();
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
-                    stringBuilder.append("\n");
-                }
-                return stringBuilder.toString();
-            case HttpURLConnection.HTTP_UNAUTHORIZED:
-                throw new UnauthorizedException();
-            default:
-                throw new IllegalStateException("Connection method is not equipped to handle this case");
-        }
-    }
-
 
 }
