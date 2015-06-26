82980fb4548ad1381ba80076de9ecdd9017339c6
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-26 23:14:09 +0200

    Cleaning up: Created MovieDatabaseServerConnector.java which handles connecting to server and throws UnauthorizedException.java if the server responds with HTTP Unauthorized (401), which usually happens if API key is not correct

---
+++ app/src/main/java/com/muhammadfarag/popularmovies/MovieDatabaseServerConnector.java
@@ -0,0 +1,63 @@
+package com.muhammadfarag.popularmovies;
+
+import android.content.Context;
+import android.net.Uri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Project: Popular Movies
+ * Created by muhammad on 26/06/15.
+ */
+class MovieDatabaseServerConnector {
+
+    private Context context;
+    private final String apikey;
+
+    public MovieDatabaseServerConnector(Context context) {
+        this.context = context;
+        this.apikey = context.getString(R.string.server_api_key);
+
+    }
+
+    public String getData() throws IOException, UnauthorizedException {
+        String baseUrl = this.context.getString(R.string.server_base_url);
+        // TODO: store string constants in resource file(s)
+        Uri uri = Uri.parse(baseUrl).buildUpon()
+                .appendQueryParameter("sort_by", "popularity.desc")
+                .appendQueryParameter("api_key", this.apikey).build();
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(uri.toString()).openConnection();
+        httpURLConnection.connect();
+
+        int responseCode;
+        try {
+            responseCode = httpURLConnection.getResponseCode();
+        } catch (IOException e) {
+            responseCode = httpURLConnection.getResponseCode();
+        }
+
+        switch (responseCode) {
+            case HttpURLConnection.HTTP_OK:
+                InputStream inputStream = httpURLConnection.getInputStream();
+                StringBuilder stringBuilder = new StringBuilder();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append("\n");
+                }
+                return stringBuilder.toString();
+            case HttpURLConnection.HTTP_UNAUTHORIZED:
+                throw new UnauthorizedException();
+            default:
+                throw new IllegalStateException("Connection method is not equipped to handle this case");
+        }
+    }
+}
