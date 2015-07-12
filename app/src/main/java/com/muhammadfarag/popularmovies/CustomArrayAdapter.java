5b02371a8482f233eb65438d887f1fc122ce6840
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-07-12 02:00:38 +0200

    Simple algorithm for loading data in array adapter dynamically... Bug: scrolling fast will result in inconsistency in the displayed tiles

--- app/src/main/java/com/muhammadfarag/popularmovies/CustomArrayAdapter.java
+++ app/src/main/java/com/muhammadfarag/popularmovies/CustomArrayAdapter.java
@@ -1,6 +1,8 @@
 package com.muhammadfarag.popularmovies;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,10 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +27,7 @@ class CustomArrayAdapter extends BaseAdapter {
     private final int resource;
     private List<Movie> elements;
     private final Object mLock = new Object();
+    private int currentPage =1;
 
     public CustomArrayAdapter(Context context, int resource, List<Movie> elements) {
         this.context = context;
@@ -30,12 +37,29 @@ class CustomArrayAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return elements.size();
+        return elements.size() == 0 ? 0 : 2000;
     }
 
     @Override
     public Movie getItem(int position) {
-        return elements.get(position);
+        Log.d("PopMovieApp", "Getting item from position[" + position +
+                "] with current page number [" + currentPage +
+                "]");
+        if(currentPage<position/20 + 1){
+            currentPage = position /20 + 1;
+            Log.d("PopMovieApp", "Increment page number to [" + currentPage +
+                    "]");
+            FetchMoviesData fetchMoviesData = new FetchMoviesData();
+            fetchMoviesData.execute(currentPage);
+        }
+        if(position !=0 && currentPage>position/20 + 1){
+            currentPage = position /20+1;
+            Log.d("PopMovieApp", "Decrement page number to [" + currentPage +
+                    "]");
+            FetchMoviesData fetchMoviesData = new FetchMoviesData();
+            fetchMoviesData.execute(currentPage);
+        }
+        return elements.get(position%20);
     }
 
     @Override
@@ -50,7 +74,7 @@ class CustomArrayAdapter extends BaseAdapter {
             view = (ImageView) LayoutInflater.from(this.context).inflate(this.resource, parent, false);
         }
 
-        String url =  getItem(position).getPosterUrl();
+        String url = getItem(position).getPosterUrl();
         Picasso.with(context).load(url).into(view);
 //        Picasso.with(context).load(albumURL).error(R.drawable.no_album_art).into(holder.cell_image);
 
@@ -63,6 +87,40 @@ class CustomArrayAdapter extends BaseAdapter {
         synchronized (mLock) {
             this.elements = elements;
         }
+        Log.e("PopMovies", "       *            *           *Dataset changed....");
         notifyDataSetChanged();
+    }
+    public void updateValuesNoNotify(List<Movie> elements) {
+        synchronized (mLock) {
+            this.elements = elements;
+        }
+    }
+
+    private class FetchMoviesData extends AsyncTask<Integer, Void, List<Movie>> {
+
+        @Override
+        protected List<Movie> doInBackground(Integer... params) {
+            MovieDatabaseServerConnector connector = new MovieDatabaseServerConnector(context);
+            Integer page = params[0];
+            Log.d("PopMovies", "Retrieving values for page [" + page +"]");
+            List<Movie> movies;
+            try {
+                movies = connector.getMovies(page, 20, 0);
+            } catch (IOException | UnauthorizedException | JSONException e) {
+                Log.e("PopMovieApp","Exception has been thrown: " + e.toString() );
+                // TODO: Display error message
+                return new ArrayList<>();
+            }
+
+            return movies;
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            if(movies.size()==0){
+                return;
+            }
+            updateValuesNoNotify(movies);
+        }
     }
 }
