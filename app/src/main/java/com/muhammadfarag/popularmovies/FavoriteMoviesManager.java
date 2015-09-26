13bf8d9649281e7477993306b1cda73dfbf097f0
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-09-26 15:37:52 +0200

    in progress: Persist the complete set of movie data instead of only its id

--- app/src/main/java/com/muhammadfarag/popularmovies/FavoriteMoviesManager.java
+++ app/src/main/java/com/muhammadfarag/popularmovies/FavoriteMoviesManager.java
@@ -6,6 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by muhammadfarag on 9/26/15.
  */
@@ -24,32 +27,55 @@ public class FavoriteMoviesManager {
         return sMoviesManager;
     }
 
-    public void add(int id) {
+    public void add(Movie movie) {
         SQLiteDatabase database = db.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put("id", id);
+        values.put("id", movie.getId());
+        values.put("title", movie.getOriginalTitle());
+        values.put("rating", movie.getUserRating());
+        values.put("release_date", movie.getReleaseDate());
+        values.put("plot", movie.getPlotSynopsis());
+        values.put("poster_url", movie.getPosterUrl());
+
         database.insert("movie", null, values);
         database.close();
     }
 
-    public void remove(int id) {
+    public void remove(Movie movie) {
         SQLiteDatabase database = db.getWritableDatabase();
-        database.delete("movie", "id = ?", new String[]{String.valueOf(id)});
+        database.delete("movie", "id = ?", new String[]{String.valueOf(movie.getId())});
         database.close();
     }
 
-    public boolean isFavorite(int id) {
+    public boolean isFavorite(Movie movie) {
         SQLiteDatabase database = db.getWritableDatabase();
         Cursor cursor = database.query("movie", null, null, null, null, null, null);
         int columnIndex = cursor.getColumnIndex("id");
         while (cursor.moveToNext()) {
-            if (cursor.getInt(columnIndex) == id) {
+            if (cursor.getInt(columnIndex) == movie.getId()) {
                 cursor.close();
                 return true;
             }
         }
         cursor.close();
         return false;
+    }
+
+    public List<Movie> getMovies() {
+        List<Movie> movies = new ArrayList<>();
+        SQLiteDatabase database = db.getWritableDatabase();
+        Cursor cursor = database.query("movie", null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            Movie movie = new Movie(cursor.getString(cursor.getColumnIndex("title")),
+                    cursor.getDouble(cursor.getColumnIndex("rating")),
+                    cursor.getString(cursor.getColumnIndex("release_date")),
+                    cursor.getString(cursor.getColumnIndex("plot")),
+                    cursor.getString(cursor.getColumnIndex("poster_url")),
+                    cursor.getInt(cursor.getColumnIndex("id")));
+            movies.add(movie);
+        }
+        cursor.close();
+        return movies;
     }
 
     private class MoviesDatabase extends SQLiteOpenHelper {
@@ -60,7 +86,7 @@ public class FavoriteMoviesManager {
 
         @Override
         public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            sqLiteDatabase.execSQL("CREATE TABLE movie(_ID INT PRIMARY KEY, id);");
+            sqLiteDatabase.execSQL("CREATE TABLE movie(_ID INT PRIMARY KEY, id, title, rating, release_date, plot, poster_url);");
         }
 
         @Override
