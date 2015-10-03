884e301381932b7fb0321495c2366726342505f6
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-10-03 20:15:10 +0200

    progress: Display list of trailers associated with fixed movie, movie id should be passed to TrailersFragment in a next step
    This was not done at this stage to facilitate debugging, since not all movies have associated trailers

--- app/src/main/java/com/muhammadfarag/popularmovies/details/TrailersFragment.java
+++ app/src/main/java/com/muhammadfarag/popularmovies/details/TrailersFragment.java
@@ -3,20 +3,61 @@ package com.muhammadfarag.popularmovies.details;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.muhammadfarag.popularmovies.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by muhammadfarag on 10/3/15.
  */
-public class TrailersFragment extends Fragment {
+public class TrailersFragment extends Fragment implements TrailersDataSetUpdateListener {
+
+    private TrailersArrayAdapter arrayAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        arrayAdapter = new TrailersArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, new HashMap<String, String>());
+
+        Map<String, String> elements = null;
+//        if (savedInstanceState != null) {
+//            elements = savedInstanceState.getParcelableArrayList("");
+//        }
+        if (elements == null) {
+            FetchTrailers fetchTrailers = new FetchTrailers(getActivity(), this);
+            fetchTrailers.execute(218);
+        } else {
+            arrayAdapter.updateValues(elements);
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trailers, container, false);
+
+        ListView gridViewLayout = (ListView) view.findViewById(R.id.trailer_list_view);
+        gridViewLayout.setAdapter(arrayAdapter);
+        gridViewLayout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("tag", "Trailer was selected but nothing to be done ");
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onDataSetUpdated(Map<String, String> trailers) {
+        arrayAdapter.updateValues(trailers);
     }
 }
